c3f71146c149fe677e35962c747b966ca8aa0c04
Antônio Edinadson Gomes Eugênio <antonioedinadson@gmail.com>
2022-07-07 19:26:57 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="text-slate-400">
+      <h1 className="text-lg">dddd</h1>
+      <h1 className="text-lg">dddd</h1>
+      <h1 className="text-lg">dddd</h1>
+    </div>
+  );
+}
+export default App
